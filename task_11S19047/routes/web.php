ab35a90ac203a843d190a98d3c8185b285fa8b4c
Albet Matthew Best Nainggolan <golanbrother@gmail.com>
2022-04-12 13:57:47 +0700

    add checkverify, edit kernel and web php

--- task_11S19047/routes/web.php
+++ task_11S19047/routes/web.php
@@ -36,7 +36,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('users', 'UserController@index');
-/*Route::get('users/profile/{id}', [UsersController::class, 'showprofile']);*/
 
 Route::middleware(['admin'])->group(function () {
     Route::get('/admin', [HomeController::class, 'admin']);
@@ -45,45 +44,36 @@ Route::middleware(['admin'])->group(function () {
 Route::middleware(['auth'])->group(function () {
     Route::get('@{username}', [UsersController::class, 'showprofile']);
     Route::put('/user/{username}', [UsersController::class, 'update']);
-    Route::resource('forum', ForumController::class);
-    Route::get('/comment/{id}/report', [ReportCommentController::class, 'reportComment']);
-    Route::get('/mark/{id}', [ForumCommentController::class, 'markcomment']);
+    Route::get('/dashboard', [FrontendController::class, 'index']);
+    Route::middleware(['verify'])->group(function () {
+        Route::middleware(['admin'])->group(function () {
+            Route::get('/admin/listpelanggaran', [AdminController::class, 'listpelanggaran']);
+            Route::get('categories', [CategoryController::class, 'index']);
+            Route::get('add-categories', [CategoryController::class, 'add']);
+            Route::post('insert-category', [CategoryController::class, 'insert']);
+            Route::get('edit-category/{id}', [CategoryController::class, 'edit']);
+            Route::put('update-category/{id}', [CategoryController::class, 'update']);
+            Route::get('delete-category/{id}', [CategoryController::class, 'destroy']);
+
+            Route::get('soal', [SoalController::class, 'index']);
+            Route::get('add-soal', [SoalController::class, 'add']);
+            Route::post('insert-soal', [SoalController::class, 'insert']);
+            Route::get('edit-soal/{id}', [SoalController::class, 'edit']);
+            Route::put('update-soal/{id}', [SoalController::class, 'update']);
+            Route::get('delete-soal/{id}', [SoalController::class, 'destroy']);
+
+            Route::get('/lab', [LabController::class, 'index']);
+            Route::get('category', [LabCategoryController::class, 'index']);
+            Route::get('view-language/{language}', [LabCategoryController::class, 'daftarsoal']);
+            Route::get('view-exercise/{id}', [LabCategoryController::class, 'lab']);
+        });
+        // nested resource
+        Route::resource('forum.comments', CommentController::class)->shallow();
+        Route::resource('forum', ForumController::class);
+        Route::get('/comment/{id}/report', [ReportCommentController::class, 'reportComment']);
+        Route::get('/mark/{id}', [ForumCommentController::class, 'markcomment']);
+    });
 });
-
-Route::resource('forum', ForumController::class)->only(['index', 'show']);
-
-// Route::middleware(['isVerify'])->group(function () {
-//     Route::resource('forum', ForumController::class);
-// });
 
 
 Route::post('ckeditor/upload', [CKEditorController::class, 'upload'])->name('ckeditor.image-upload')->middleware('auth');
-
-// nested resource
-Route::resource('forum.comments', CommentController::class)->shallow()->middleware('auth');
-
-//Lab Coding
-Route::get('/dashboard', [FrontendController::class, 'index']);
-
-Route::get('categories', [CategoryController::class, 'index']);
-Route::get('add-categories', [CategoryController::class, 'add']);
-Route::post('insert-category', [CategoryController::class, 'insert']);
-Route::get('edit-category/{id}', [CategoryController::class, 'edit']);
-Route::put('update-category/{id}', [CategoryController::class, 'update']);
-Route::get('delete-category/{id}', [CategoryController::class, 'destroy']);
-
-
-Route::get('soal', [SoalController::class, 'index']);
-Route::get('add-soal', [SoalController::class, 'add']);
-Route::post('insert-soal', [SoalController::class, 'insert']);
-Route::get('edit-soal/{id}', [SoalController::class, 'edit']);
-Route::put('update-soal/{id}', [SoalController::class, 'update']);
-Route::get('delete-soal/{id}', [SoalController::class, 'destroy']);
-
-Route::get('/lab', [LabController::class, 'index']);
-Route::get('category', [LabCategoryController::class, 'index']);
-Route::get('view-language/{language}', [LabCategoryController::class, 'daftarsoal']);
-Route::get('view-exercise/{id}', [LabCategoryController::class, 'lab']);
-
-
-Route::get('/admin/listpelanggaran', [AdminController::class, 'listpelanggaran']);
