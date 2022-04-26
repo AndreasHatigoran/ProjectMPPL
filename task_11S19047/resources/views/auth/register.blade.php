b147bb2961579aadd43d23078d015a48dd6b68af
ifs19048 <71029236+ifs19048@users.noreply.github.com>
2022-04-26 14:34:33 +0700

    updated

--- task_11S19047/resources/views/auth/register.blade.php
+++ task_11S19047/resources/views/auth/register.blade.php
@@ -36,7 +36,6 @@
                               </select>
                             </div>
                         </div>
-
                         <div class="row mb-3">
                             <label for="idrole" class="col-md-4 col-form-label text-md-end">NIM/NIDN/NIP</label>
 
