f9899aecb5b97c3c4e4dc230bcb73666d3e495d2
Albet Matthew Best Nainggolan <golanbrother@gmail.com>
2022-05-07 15:25:27 +0700

    Update main.blade.php

--- task_11S19047/resources/views/layouts/main.blade.php
+++ task_11S19047/resources/views/layouts/main.blade.php
@@ -208,7 +208,7 @@
                                                     <path fill-rule="evenodd"
                                                         d="M4.72 3.22a.75.75 0 011.06 1.06L2.06 8l3.72 3.72a.75.75 0 11-1.06 1.06L.47 8.53a.75.75 0 010-1.06l4.25-4.25zm6.56 0a.75.75 0 10-1.06 1.06L13.94 8l-3.72 3.72a.75.75 0 101.06 1.06l4.25-4.25a.75.75 0 000-1.06l-4.25-4.25z">
                                                     </path>
-                                                </svg><span>Lab Coding</span></a>
+                                                </svg><span>Lab Code</span></a>
                                         </li>
                                         <li class="dropdown"><a class="nav-link menu-title link-nav"
                                                 href="../admin/listpelanggaran"><i data-feather="file"></i><span>List
