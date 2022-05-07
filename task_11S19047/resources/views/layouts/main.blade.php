ea8f2edadd489ccf93f30ee3c717fdf38120dfcf
Albet Matthew Best Nainggolan <golanbrother@gmail.com>
2022-05-07 15:18:05 +0700

    Update main.blade.php
    i fixed the css

--- task_11S19047/resources/views/layouts/main.blade.php
+++ task_11S19047/resources/views/layouts/main.blade.php
@@ -80,27 +80,26 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
         rel="stylesheet">
-    <!-- Font Awesome-->
-    <link rel="stylesheet" type="text/css" href="../css/welcome/fontawesome.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome/fontawesome.css') }}">
     <!-- ico-font-->
-    <link rel="stylesheet" type="text/css" href="../css/welcome/icofont.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome/icofont.css') }}">
     <!-- Themify icon-->
-    <link rel="stylesheet" type="text/css" href="../css/welcome/themify.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome/themify.css') }}">
+
     <!-- Flag icon-->
-    <link rel="stylesheet" type="text/css" href="../css/welcome/flag-icon.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome/flag-icon.css') }}">
     <!-- Feather icon-->
-    <link rel="stylesheet" type="text/css" href="../css/welcome/feather-icon.css">
-    <!-- Plugins css start-->
-    {{-- <link rel="stylesheet" type="text/css" href="../css/welcome/select2.css">
-    <link rel="stylesheet" type="text/css" href="../css/welcome/dropzone.css"> --}}
-    <!-- Plugins css Ends-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome/feather-icon.css') }}">
+
+
     <!-- Bootstrap css-->
-    <link rel="stylesheet" type="text/css" href="../css/welcome/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome/bootstrap.css') }}">
+
     <!-- App css-->
-    <link rel="stylesheet" type="text/css" href="../css/welcome/style.css">
-    <link id="color" rel="stylesheet" href="../css/welcome/color-1.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome/style.css') }}">
+    <link id="color" rel="stylesheet href=" {{ asset('css/welcome/color-1.css') }}" media="screen">
     <!-- Responsive css-->
-    <link rel="stylesheet" type="text/css" href="../css/welcome/responsive.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome/responsive.css') }}">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
@@ -360,31 +359,21 @@
             </footer>
         </div>
     </div>
-    <!-- latest jquery-->
-    <script src="../js/welcome/jquery-3.5.1.min.js"></script>
+    <script src="{{ asset('js/welcome/jquery-3.5.1.min.js') }}"></script>
     <!-- feather icon js-->
-    <script src="../js/welcome/icons/feather-icon/feather.min.js"></script>
-    <script src="../js/welcome/icons/feather-icon/feather-icon.js"></script>
+    <script src="{{ asset('js/welcome/icons/feather-icon/feather.min.js') }}"></script>
+    <script src="{{ asset('js/welcome/icons/feather-icon/feather-icon.js') }}"></script>
     <!-- Sidebar jquery-->
-    <script src="../js/welcome/sidebar-menu.js"></script>
-    <script src="../js/welcome/config.js"></script>
+    <script src="{{ asset('js/welcome/sidebar-menu.js') }}"></script>
+    <script src="{{ asset('js/welcome/config.js') }}"></script>
+
     <!-- Bootstrap js-->
-    <script src="../js/welcome/bootstrap/popper.min.js"></script>
-    <script src="../js/welcome/bootstrap/bootstrap.min.js"></script>
-    <!-- Plugins JS start-->
-    {{-- <script src="../js/welcome/editor/ckeditor/ckeditor.js"></script>
-            <script src="../js/welcome/editor/ckeditor/adapters/jquery.js"></script>
-            <script src="../js/welcome/dropzone/dropzone.js"></script>
-            <script src="../js/welcome/dropzone/dropzone-script.js"></script>
-            <script src="../js/welcome/select2/select2.full.min.js"></script>
-            <script src="../js/welcome/select2/select2-custom.js"></script>
-            <script src="../js/welcome/email-app.js"></script>
-            <script src="../js/welcome/form-validation-custom.js"></script> --}}
-    <!-- Plugins JS Ends-->
-    <!-- Theme js-->
-    <script src="../js/welcome/script.js"></script>
-    <!-- login js-->
-    <!-- Plugin used-->
+    <script src="{{ asset('js/welcome/bootstrap/popper.min.js') }}"></script>
+    <script src="{{ asset('js/welcome/bootstrap/bootstrap.min.js') }}"></script>
+
+    <script src="{{ asset('js/welcome/script.js') }}"></script>
+
+</body>
 </body>
 
 
