d247746a35d377781ff90c5291e959eb343b2b23
ifs19048 <71029236+ifs19048@users.noreply.github.com>
2022-05-10 14:26:34 +0700

    Update edit.blade.php

--- task_11S19047/resources/views/forum/edit.blade.php
+++ task_11S19047/resources/views/forum/edit.blade.php
@@ -4,27 +4,23 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="page-header">
             <div class="row">
+             <div class="card">
                 <div class="card-header">
                     <h1>Edit Artikel</h1>
                 </div>
-            </div>
-        </div>
-    </div>
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-md-12">
-                <div class="card">
 
                     <div class="card-body">
                         <form action="/forum/{{ $forum->id }}" method="POST">
                             @csrf
                             @method('PUT')
                             <x-input field="title" label="Judul" type="text" value="{{ $forum->title }}" />
-                            <x-ckeditor field="subject" label="Subject" value="{{ $forum->subject }}" />
+                            <x-ckeditor field="subject" label="Pertanyaan" value="{{ $forum->subject }}" />
 
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" class="btn btn-primary">Kirim</button>
                         </form>
                     </div>
                 </div>
